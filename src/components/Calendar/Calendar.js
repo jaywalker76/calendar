a457c002b06cf1605c8087274bf973b4a92a6bf6
Julio Costa <juliocosta1@gmail.com>
2019-12-13 20:50:31 +0000

    fix tests for basic calendar structure

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -3,9 +3,12 @@ import React from "react";
 const Calendar = () => {
   return (
     <div data-test="calendar-component">
-      <div id="header">Header</div>
-      <div>Days</div>
-      <div>Cells</div>
+      <div id="header" data-test="calendar-header">
+        Header
+      </div>
+      <div data-test="calendar-current-date">Current Date</div>
+      <div data-test="calendar-days">Days</div>
+      <div data-test="calendar-cells">Cells</div>
     </div>
   );
 };
