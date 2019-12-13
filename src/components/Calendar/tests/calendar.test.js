956852bf0ac43d1221f5ec66ba0a2128ebb405a9
Julio Costa <juliocosta1@gmail.com>
2019-12-13 20:47:24 +0000

    add remaining test cases for component

--- src/components/Calendar/tests/calendar.test.js
+++ src/components/Calendar/tests/calendar.test.js
@@ -6,17 +6,37 @@ import Calendar from "../Calendar";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-it("renders without crashing", () => {
-  const wrapper = shallow(<Calendar />);
-  expect(wrapper).toBeTruthy();
-});
+describe("Calendar structure", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<Calendar />);
+    expect(wrapper).toBeTruthy();
+  });
 
-it("renders the calendar structure", () => {
-  const wrapper = shallow(<Calendar />);
-  const calendarComponent = wrapper.find("[data-test='calendar-component']");
-  expect(calendarComponent.length).toBe(1);
+  it("renders the calendar structure", () => {
+    const wrapper = shallow(<Calendar />);
+    const calendarComponent = wrapper.find("[data-test='calendar-component']");
+    expect(calendarComponent.length).toBe(1);
+  });
+  it("displays the header", () => {
+    const wrapper = shallow(<Calendar />);
+    const calendarComponent = wrapper.find("[data-test='calendar-header']");
+    expect(calendarComponent.length).toBe(1);
+  });
+  it("displays the days", () => {
+    const wrapper = shallow(<Calendar />);
+    const calendarComponent = wrapper.find("[data-test='calendar-days']");
+    expect(calendarComponent.length).toBe(1);
+  });
+  it("displays the cells", () => {
+    const wrapper = shallow(<Calendar />);
+    const calendarComponent = wrapper.find("[data-test='calendar-cells']");
+    expect(calendarComponent.length).toBe(1);
+  });
+  it("displays current date date", () => {
+    const wrapper = shallow(<Calendar />);
+    const calendarComponent = wrapper.find(
+      "[data-test='calendar-current-date']"
+    );
+    expect(calendarComponent.length).toBe(1);
+  });
 });
-it("displays the header", () => {});
-it("displays the days", () => {});
-it("displays the cells", () => {});
-it("displays current date date", () => {});
