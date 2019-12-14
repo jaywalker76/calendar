b22bccc477414d62975983b1d43e7c9f05f6d1b2
Julio Costa <juliocosta1@gmail.com>
2019-12-14 12:42:41 +0000

    add basic styling to calendar structure

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,16 +1,31 @@
 import React from "react";
+import injectSheet from "react-jss";
+
+const styling = {
+  outerWrapper: {
+    width: 600
+  },
+  calendarWrapper: {
+    width: "100%",
+    border: "1px solid black"
+  }
+};
 
 const Calendar = () => {
   return (
-    <div data-test="calendar-component">
-      <div id="header" data-test="calendar-header">
-        Header
+    <div style={styling.outerWrapper}>
+      <div style={styling.calendarWrapper} data-test="calendar-component">
+        <div id="header" data-test="calendar-header">
+          Header
+        </div>
+        <div data-test="calendar-current-date">Current Date</div>
+        <div data-test="calendar-days">Days</div>
+        <div data-test="calendar-cells">Cells</div>
       </div>
-      <div data-test="calendar-current-date">Current Date</div>
-      <div data-test="calendar-days">Days</div>
-      <div data-test="calendar-cells">Cells</div>
     </div>
   );
 };
+
+// const StyledCalendar = injectSheet(styles)(Calendar);
 
 export default Calendar;
