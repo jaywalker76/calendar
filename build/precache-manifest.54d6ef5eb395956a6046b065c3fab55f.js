c562f437a022ccb609fffac15381116b27cabe6b
Julio Costa <juliocosta1@gmail.com>
2019-12-14 12:18:08 +0000

    refactor test structure

---
+++ build/precache-manifest.54d6ef5eb395956a6046b065c3fab55f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80121982daf5733efbb2a73cbf2a8927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0827dd4cf6e8b7d368bd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c0ba5ff7031e210880a",
+    "url": "/static/js/2.29f44ddb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29f44ddb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0827dd4cf6e8b7d368bd",
+    "url": "/static/js/main.6ff09862.chunk.js"
+  },
+  {
+    "revision": "ac79b348ddaa6cd06908",
+    "url": "/static/js/runtime-main.ae502b27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/components/Calendar/tests/calendar.test.js
+++ src/components/Calendar/tests/calendar.test.js
@@ -6,34 +6,47 @@ import Calendar from "../Calendar";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * [Factory function to create a ShallowWrapper for the App Component]
+ * @function setup
+ * @param  {Object} [props={}]   [props to pass to component]
+ * @param  {[type]} [state=null] [component count state]
+ * @return {[type]}              [shallow App]
+ */
+const setup = (props = {}, state = null) => {
+  return shallow(<Calendar {...props} />);
+};
+
+let wrapper;
+
 describe("Calendar structure", () => {
   it("renders without crashing", () => {
-    const wrapper = shallow(<Calendar />);
+    wrapper = setup();
     expect(wrapper).toBeTruthy();
   });
 
   it("renders the calendar structure", () => {
-    const wrapper = shallow(<Calendar />);
+    wrapper = setup();
     const calendarComponent = wrapper.find("[data-test='calendar-component']");
     expect(calendarComponent.length).toBe(1);
   });
   it("displays the header", () => {
-    const wrapper = shallow(<Calendar />);
+    wrapper = setup();
     const calendarComponent = wrapper.find("[data-test='calendar-header']");
     expect(calendarComponent.length).toBe(1);
   });
   it("displays the days", () => {
-    const wrapper = shallow(<Calendar />);
+    wrapper = setup();
     const calendarComponent = wrapper.find("[data-test='calendar-days']");
     expect(calendarComponent.length).toBe(1);
   });
   it("displays the cells", () => {
-    const wrapper = shallow(<Calendar />);
+    wrapper = setup();
     const calendarComponent = wrapper.find("[data-test='calendar-cells']");
     expect(calendarComponent.length).toBe(1);
   });
   it("displays current date date", () => {
-    const wrapper = shallow(<Calendar />);
+    wrapper = setup();
     const calendarComponent = wrapper.find(
       "[data-test='calendar-current-date']"
     );
