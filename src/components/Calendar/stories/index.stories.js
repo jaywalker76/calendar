3f4675336ff85eb8571f4c0cd26af67c38af9ac4
Julio Costa <juliocosta1@gmail.com>
2020-01-23 19:22:39 +0000

    added story for calendar with no header

--- src/components/Calendar/stories/index.stories.js
+++ src/components/Calendar/stories/index.stories.js
@@ -7,6 +7,9 @@ export default { title: "Calendar" };
 export const defaultCalendar = () => (
   <Calendar includeHeader={true} displayNavArrows={true} />
 );
+export const calendarWithNoHeader = () => (
+  <Calendar startDate={"2019/12/01"} includeHeader={false} />
+);
 export const withSpecifiedStartDate = () => (
   <Calendar
     startDate={"2019/12/01"}
