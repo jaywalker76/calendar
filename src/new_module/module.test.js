ed98f84677a08358a874d976a67f85ce1bc4e627
Julio Costa <juliocosta1@gmail.com>
2020-02-14 21:17:03 +0000

    added test id message

--- src/new_module/module.test.js
+++ src/new_module/module.test.js
@@ -2,13 +2,13 @@ import CalendarModule from "./Module";
 
 describe("Module functionality", () => {
   test.each`
-    dateValue       | expectedResult
-    ${"10/01/2019"} | ${"October, 2019"}
-    ${"12/31/2019"} | ${"December, 2019"}
-    ${"01/01/2020"} | ${"January, 2020"}
-    ${"02/29/2020"} | ${"February, 2020"}
+    testName                                   | dateValue       | expectedResult
+    ${"Date String generation with Param set"} | ${"10/01/2019"} | ${"October, 2019"}
+    ${"Date String generation with Param set"} | ${"12/31/2019"} | ${"December, 2019"}
+    ${"Date String generation with Param set"} | ${"01/01/2020"} | ${"January, 2020"}
+    ${"Date String generation with Param set"} | ${"02/29/2020"} | ${"February, 2020"}
   `(
-    "correctly converts $dateValue to $expectedResult",
+    "$testName: correctly converts $dateValue to $expectedResult",
     ({ dateValue, expectedResult }) => {
       const moduleInstance = new CalendarModule(dateValue);
       expect(moduleInstance.dateString).toBe(expectedResult);
