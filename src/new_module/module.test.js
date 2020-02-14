09ef12df436739c9a0c3495b7dcb62ebc875128b
Julio Costa <juliocosta1@gmail.com>
2020-02-14 21:22:15 +0000

    added test for date string with no date param

--- src/new_module/module.test.js
+++ src/new_module/module.test.js
@@ -14,4 +14,13 @@ describe("Module functionality", () => {
       expect(moduleInstance.dateString).toBe(expectedResult);
     }
   );
+
+  it("Displays string for current date when no date param is set", () => {
+    const dateObj = new Date();
+    const monthName = dateObj.toLocaleString("default", { month: "long" });
+    const yearString = dateObj.getFullYear();
+
+    const moduleInstance = new CalendarModule();
+    expect(moduleInstance.dateString).toBe(`${monthName}, ${yearString}`);
+  });
 });
