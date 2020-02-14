c81563b7525af04bafd7832e0c4b980996a05026
Julio Costa <juliocosta1@gmail.com>
2020-02-14 20:12:17 +0000

    added test for date string to match date param

--- src/new_module/module.test.js
+++ src/new_module/module.test.js
@@ -1,3 +1,11 @@
 import CalendarModule from "./Module";
 
-describe("Module functionality", () => {});
+describe("Module functionality", () => {
+  const ModuleInstance = new CalendarModule("01/10/2019");
+
+  it("date string should match date param", () => {
+    const dateString = ModuleInstance.dateString;
+
+    expect(dateString).toMatch("October, 2019");
+  });
+});
