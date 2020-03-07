06dac6b27dcd4d25b98dad3afe8c03970e20de2e
Julio Costa <juliocosta1@gmail.com>
2020-03-07 16:20:27 +0000

    implement changes for ed98f8467

--- src/new_module/module.test.js
+++ src/new_module/module.test.js
@@ -14,13 +14,13 @@ import { monthTest } from "./day_months";
 
 describe("Module functionality", () => {
   test.each`
-    testName                                   | dateValue       | expectedResult
-    ${"Date String generation with Param set"} | ${"10/01/2019"} | ${"October, 2019"}
-    ${"Date String generation with Param set"} | ${"12/31/2019"} | ${"December, 2019"}
-    ${"Date String generation with Param set"} | ${"01/01/2020"} | ${"January, 2020"}
-    ${"Date String generation with Param set"} | ${"02/29/2020"} | ${"February, 2020"}
+    dateValue       | expectedResult
+    ${"10/01/2019"} | ${"October, 2019"}
+    ${"12/31/2019"} | ${"December, 2019"}
+    ${"01/01/2020"} | ${"January, 2020"}
+    ${"02/29/2020"} | ${"February, 2020"}
   `(
-    "$testName: correctly converts $dateValue to $expectedResult",
+    "Date String generation with Param set: correctly converts $dateValue to $expectedResult",
     ({ dateValue, expectedResult }) => {
       const moduleInstance = new CalendarModule(dateValue);
       expect(moduleInstance.dateString).toBe(expectedResult);
