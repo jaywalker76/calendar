31494032526104fd8d673f0bd9b4e07ddf10a56a
Julio Costa <juliocosta1@gmail.com>
2020-03-24 19:53:28 +0000

    add file with month test cases

---
+++ src/new_module/month_objects.js
@@ -0,0 +1,22 @@
+// March starts on a sunday, has no days from previous month
+const March2020 = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+]
+// Feb 28 days, starting on a Sunday
+const February2015 = [
+    {},
+    {},
+    {},
+    {},
+    {}
+]
+// April 2020 having days from previous and next month
+
+next test -> days at the beginning of month, with days of next month at end (days at end) March
+next test -> days at end of month, with days of previous month at beginnning (days at start)Feb
+next test -> month with both cases -> April
