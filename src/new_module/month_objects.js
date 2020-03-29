5fbf51521e4e43deae06dc8caa35979612593679
Julio Costa <juliocosta1@gmail.com>
2020-03-29 16:24:37 +0100

    add test case for feb2015 - month with no trailing or leading days with 4 weeks

--- src/new_module/month_objects.js
+++ src/new_module/month_objects.js
@@ -1,5 +1,43 @@
 // Feb 28 days, starting on a Sunday
-const February2015 = [{}, {}, {}, {}, {}];
+// with no leading and trailing days and having just 4 weeks
+const February2015 = [
+  [
+    { day: 1, weekday: 0, currentMonth: true },
+    { day: 2, weekday: 1, currentMonth: true },
+    { day: 3, weekday: 2, currentMonth: true },
+    { day: 4, weekday: 3, currentMonth: true },
+    { day: 5, weekday: 4, currentMonth: true },
+    { day: 6, weekday: 5, currentMonth: true },
+    { day: 7, weekday: 6, currentMonth: true }
+  ],
+  [
+    { day: 8, weekday: 0, currentMonth: true },
+    { day: 9, weekday: 1, currentMonth: true },
+    { day: 10, weekday: 2, currentMonth: true },
+    { day: 11, weekday: 3, currentMonth: true },
+    { day: 12, weekday: 4, currentMonth: true },
+    { day: 13, weekday: 5, currentMonth: true },
+    { day: 14, weekday: 6, currentMonth: true }
+  ],
+  [
+    { day: 15, weekday: 0, currentMonth: true },
+    { day: 16, weekday: 1, currentMonth: true },
+    { day: 17, weekday: 2, currentMonth: true },
+    { day: 18, weekday: 3, currentMonth: true },
+    { day: 19, weekday: 4, currentMonth: true },
+    { day: 20, weekday: 5, currentMonth: true },
+    { day: 21, weekday: 6, currentMonth: true }
+  ],
+  [
+    { day: 22, weekday: 0, currentMonth: true },
+    { day: 23, weekday: 1, currentMonth: true },
+    { day: 24, weekday: 2, currentMonth: true },
+    { day: 25, weekday: 3, currentMonth: true },
+    { day: 26, weekday: 4, currentMonth: true },
+    { day: 27, weekday: 5, currentMonth: true },
+    { day: 28, weekday: 6, currentMonth: true }
+  ]
+];
 // Feb 2019 has 28 days
 const February2019 = [{}, {}, {}, {}, {}];
 // Feb2020 has leading days, no trailing days
