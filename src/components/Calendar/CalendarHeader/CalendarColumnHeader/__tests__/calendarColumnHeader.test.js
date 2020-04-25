8841998be4b9bbe3621e7cd84b361ee28e9ae3a8
Julio Costa <juliocosta1@gmail.com>
2020-04-25 12:46:05 +0100

    add index to new_module and add test for default cal col header

---
+++ src/components/Calendar/CalendarHeader/CalendarColumnHeader/__tests__/calendarColumnHeader.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+import CalendarModule from "../../../../../new_module/";
+import CalendarColumnHeader from "..";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * [Factory function to create a ShallowWrapper for the App Component]
+ * @function setup
+ * @param  {Object} [props={}]   [props to pass to component]
+ * @param  {[type]} [state=null] [component count state]
+ * @return {[type]}              [shallow App]
+ */
+
+const setup = (props = {}, state = null) => {
+  return mount(<CalendarColumnHeader {...props} />);
+};
+
+let wrapper;
+
+describe("Calendar Column Header Structure", () => {
+  let startDate = "04/01/2020";
+  const calendarModule = new CalendarModule(startDate);
+  const retrievedWeekHeaderObject = calendarModule.getWeekHeader();
+
+  let props = { weekHeaderObject: retrievedWeekHeaderObject };
+
+  it("renders without crashing", () => {
+    wrapper = setup(props);
+    expect(wrapper).toBeTruthy();
+  });
+});
