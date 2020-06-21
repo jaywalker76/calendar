b6d91f638a9a45ad2ae798ec15fbbda794c383c0
JSCC <juliocosta1@gmail.com>
2020-06-21 09:25:53 +0100

    test for event creation in new event store
    Easier to order ideas starting with a new Event store

--- src/Events/EventStore.js
+++ src/Events/EventStore.js
@@ -6,6 +6,12 @@ module.exports = class EventStore {
     this.eventList = eventList === undefined ? {} : eventList;
   }
   // create event
+  createEvent(eventToAdd) {
+    const existingEvents = Object.keys(this.eventList).length;
+    const newEventIndex = existingEvents === 0 ? 0 : existingEvents + 1;
+    this.eventList[newEventIndex] = eventToAdd;
+    return this.eventList;
+  }
   // read event
   // update event
   // delete event
--- src/Events/eventstore.test.js
+++ src/Events/eventstore.test.js
@@ -10,4 +10,20 @@ describe("Module functionality", () => {
     eventStoreInstance = setup(events);
     expect(eventStoreInstance).toBeTruthy();
   });
+
+  it("Retrieves EventStore instance when there are no existing events", () => {
+    eventStoreInstance = setup();
+    expect(eventStoreInstance).toBeTruthy();
+  });
+  // create event
+  it("Create an event in the EventStore instance", () => {
+    eventStoreInstance = setup();
+    const newEvent = { id: 1, startDate: "2020-01-01", endDate: "2020-01-01" };
+    let updatedList = eventStoreInstance.createEvent(newEvent);
+    expect(updatedList[0]).toEqual(newEvent);
+  });
+
+  // read event
+  // update event
+  // delete event
 });
