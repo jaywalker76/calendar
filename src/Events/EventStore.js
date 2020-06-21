ee3c046300cec0b5215babd1f13932a1d0715ce1
JSCC <juliocosta1@gmail.com>
2020-06-21 10:59:25 +0100

    redefine how event store is expressed

--- src/Events/EventStore.js
+++ src/Events/EventStore.js
@@ -3,13 +3,11 @@ This module is responsible for handling the crud operations related to calendar 
 */
 module.exports = class EventStore {
   constructor(eventList) {
-    this.eventList = eventList === undefined ? {} : eventList;
+    this.eventList = eventList === undefined ? [] : eventList;
   }
   // create event
   createEvent(eventToAdd) {
-    const existingEvents = Object.keys(this.eventList).length;
-    const newEventIndex = existingEvents === 0 ? 0 : existingEvents + 1;
-    this.eventList[newEventIndex] = eventToAdd;
+    this.eventList.push(eventToAdd);
     return this.eventList;
   }
   // read event
