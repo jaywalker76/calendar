66b68fcb97ba231682952f5228de2d9f9bd2fe4e
JSCC <juliocosta1@gmail.com>
2020-06-21 09:41:09 +0100

    test for event creation and update

--- src/Events/eventstore.test.js
+++ src/Events/eventstore.test.js
@@ -22,7 +22,20 @@ describe("Module functionality", () => {
     let updatedList = eventStoreInstance.createEvent(newEvent);
     expect(updatedList[0]).toEqual(newEvent);
   });
+  it("Creates and updates event", () => {
+    eventStoreInstance = setup();
+    const newEvent = { id: 1, startDate: "2020-01-01", endDate: "2020-01-01" };
+    const updateToEvent = {
+      id: 1,
+      startDate: "2021-01-01",
+      endDate: "2021-01-01",
+    };
+    let updatedList = eventStoreInstance.createEvent(newEvent);
+    expect(updatedList[0]).toEqual(newEvent);
 
+    updatedList = eventStoreInstance.updateEvent(updatedList[0], updateToEvent);
+    expect(updatedList[0]).toEqual(updateToEvent);
+  });
   // read event
   // update event
   // delete event
