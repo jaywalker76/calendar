035620f506c7d525ad855a4dbf7b4bd346c84ffc
JSCC <juliocosta1@gmail.com>
2020-07-27 19:39:42 +0100

    refactor mock event store
    convert object to array

--- src/Events/events.js
+++ src/Events/events.js
@@ -1,9 +1,7 @@
-const events = {
-  events: [
-    { id: 1, startDate: "2020-04-01", endDate: "2020-04-01" },
-    { id: 2, startDate: "2020-04-02", endDate: "2020-04-02" },
-    { id: 3, startDate: "2020-04-01", endDate: "2020-04-02" },
-  ],
-};
+const events = [
+  { id: 1, startDate: "2020-04-01", endDate: "2020-04-01" },
+  { id: 2, startDate: "2020-04-02", endDate: "2020-04-02" },
+  { id: 3, startDate: "2020-04-01", endDate: "2020-04-02" },
+];
 
 export default events;
