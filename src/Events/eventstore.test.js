1cf82d9a6fe30d517f9031a902f56c643e19ebbd
JSCC <juliocosta1@gmail.com>
2020-07-28 22:13:55 +0100

    wip - refactor tests

--- src/Events/eventstore.test.js
+++ src/Events/eventstore.test.js
@@ -23,8 +23,6 @@ let omit = (obj, props) => {
   return newObj;
 };
 
-let eventStoreInstance;
-
 // tasks to execute:
 // update test procedure
 // - create
@@ -35,14 +33,31 @@ let eventStoreInstance;
 // ToDo - test object deletion by trying to retrieve the deleted object
 // ToDo - test object creation by trying to retrieve the created object
 // ToDo - test object update by trying to retrieve the updated object
+
+const mockEventGenerator = (providedEventStore, numberOfEvents) => {
+  let createdEventIds = [];
+
+  for (let i = 0; i < numberOfEvents; i++) {
+    let dayCount = i + 1;
+    if (dayCount < 10) {
+      dayCount = "0" + dayCount;
+    }
+    let createdEvent = providedEventStore.createEvent({
+      startDate: `2021-01-${dayCount}`,
+      endDate: `2021-01-${dayCount}`,
+    });
+    createdEventIds.push(createdEvent.id);
+  }
+};
+
 describe("Module functionality with empty event store", () => {
   it("Retrieves EventStore instance when there are no existing events", () => {
-    eventStoreInstance = setup();
+    let eventStoreInstance = setup();
     expect(eventStoreInstance).toBeTruthy();
   });
   // create event
   it("Create an event in the EventStore instance", () => {
-    eventStoreInstance = setup();
+    let eventStoreInstance = setup();
     const newEventData = { startDate: "2020-01-01", endDate: "2020-01-01" };
     let createdEvent = eventStoreInstance.createEvent(newEventData);
     // read new created event
@@ -53,47 +68,44 @@ describe("Module functionality with empty event store", () => {
   });
 
   it("Creates and updates event", () => {
-    eventStoreInstance = setup();
+    let eventStoreInstance = setup();
     const newEvent = { startDate: "2020-01-01", endDate: "2020-01-01" };
+
+    let createdEvent = eventStoreInstance.createEvent(newEvent);
+    let createdEventID = createdEvent.id;
+
+    let createdEventInStore = eventStoreInstance.readEventById(createdEventID);
+
+    createdEventInStore = omit(createdEventInStore, "id");
+
+    expect(createdEventInStore).toEqual(newEvent);
+
     const updateToEvent = {
+      id: createdEventID,
       startDate: "2021-01-01",
       endDate: "2021-01-01",
     };
-    let createdEvent = eventStoreInstance.createEvent(newEvent);
-
-    let createdEventInStore = eventStoreInstance.readEventById(createdEvent.id);
-    delete createdEventInStore["id"];
-    expect(createdEventInStore).toEqual(newEvent);
 
     eventStoreInstance.updateEvent(updateToEvent);
 
-    createdEventInStore = eventStoreInstance.readEventById(createdEvent.id);
+    let updatedEventInStore = eventStoreInstance.readEventById(createdEventID);
 
-    expect(createdEventInStore).toEqual(updateToEvent);
+    expect(updatedEventInStore).toEqual(updateToEvent);
   });
   // read event
   it("Creates multiple events and reads events in a given date range", () => {
-    eventStoreInstance = setup();
+    let eventStoreInstance = setup();
 
-    for (let i = 0; i < 10; i++) {
-      let dayCount = i + 1;
-      if (dayCount < 10) {
-        dayCount = "0" + dayCount;
-      }
-      eventStoreInstance.createEvent({
-        startDate: `2021-01-${dayCount}`,
-        endDate: `2021-01-${dayCount}`,
-      });
-    }
+    let createdEventIds = mockEventGenerator(eventStoreInstance, 10);
 
     // specifying the range in an object seems better as a way to
     // enforce date sequence
-    let retrievedList = eventStoreInstance.readEvents({
+    let retrievedEvents = eventStoreInstance.readEvents({
       startDate: "2021-01-01",
       endDate: "2021-01-04",
     });
 
-    expect(retrievedList.length).toBe(4);
+    expect(retrievedEvents.length).toBe(4);
 
     const comparisonObj = [
       {
@@ -114,41 +126,33 @@ describe("Module functionality with empty event store", () => {
       },
     ];
 
-    retrievedList.forEach((calEvent) => {
-      delete calEvent["id"];
+    let idStrippedEvents = [];
+    retrievedEvents.forEach((calEvent) => {
+      idStrippedEvents.push(omit(calEvent, "id"));
     });
 
-    expect(comparisonObj[0]).toMatchObject(retrievedList[0]);
+    expect(comparisonObj[0]).toMatchObject(idStrippedEvents[0]);
   });
 
   it("Creates multiple events and retrieves a specific event by id", () => {
-    eventStoreInstance = setup();
+    let eventStoreInstance = setup();
 
-    for (let i = 0; i < 5; i++) {
-      let dayCount = i + 1;
-      if (dayCount < 10) {
-        dayCount = "0" + dayCount;
-      }
-      eventStoreInstance.createEvent({
-        startDate: `2021-01-${dayCount}`,
-        endDate: `2021-01-${dayCount}`,
-      });
-    }
+    let createdEventIds = mockEventGenerator(eventStoreInstance, 10);
 
     let updatedList = eventStoreInstance.readEventById(3);
 
-    expect(updatedList).toEqual([
-      {
-        id: 3,
-        startDate: "2021-01-03",
-        endDate: "2021-01-03",
-      },
-    ]);
+    expect(updatedList).toMatchObject({
+      id: 3,
+      startDate: "2021-01-03",
+      endDate: "2021-01-03",
+    });
   });
 
   // delete event
   it("Create Events and deletes event by id", () => {
-    eventStoreInstance = setup();
+    let eventStoreInstance = setup();
+
+    // get list of ids for generated events
 
     for (let i = 0; i < 5; i++) {
       let dayCount = i + 1;
@@ -174,12 +178,19 @@ describe("Module functionality with empty event store", () => {
 
     // delete first and last event
     eventStoreInstance.deleteEvent(1);
+
+    // test for retrieval of deleted event -> should throw error
+
     eventStoreInstance.deleteEvent(5);
+
+    // test for retrieval of deleted event -> should throw error
 
     expect(eventStoreInstance.eventList).toMatchObject(first_last_deletion);
 
     // delete middle event in list
     eventStoreInstance.deleteEvent(3);
+
+    // test for retrieval of deleted event -> should throw error
     expect(eventStoreInstance.eventList).toMatchObject(middle_deletion);
   });
 });
